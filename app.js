5f54892d3a22448f2e8ff4b40aeb91d15fb50fe9
ErnLans <lans1965ern@gmail.com>
2021-10-21 09:23:49 +0300

    cycles v-for

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 Vue.createApp({
     data: () => ({
         myHtml: '<h1>Vue 3 App</h1>',
-        title: 'проверочка'
+        title: 'проверочка',
+        person: {
+            firstName: ' Ernest',
+            lastName: ' Lans',
+            ade: 56
+        },
+        items: [1,2,3,4,5,6]
     })
 }).mount('#app')
