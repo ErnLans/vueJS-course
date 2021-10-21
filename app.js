798f375ed9916292e033819edac5a72810ac5588
ErnLans <lans1965ern@gmail.com>
2021-10-21 12:36:09 +0300

    06-key

--- app.js
+++ app.js
@@ -9,4 +9,9 @@ Vue.createApp({
         },
         items: [1,2,3,4,5,6]
     })
+/*    methods: {
+        stopPropagation(event) {
+            event.stopPropagation()
+        }
+    }*/
 }).mount('#app')
