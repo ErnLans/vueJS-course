682aa80c2c87fa9be9091e6028d12dc48161143a
ErnLans <lans1965ern@gmail.com>
2021-10-21 15:00:47 +0300

    09-some functions

--- app.js
+++ app.js
@@ -9,13 +9,19 @@ Vue.createApp({
             },
             items: [1, 2]
         }),
-    methods: {
-            addItem(event){
+        methods: {
+            addItem(event) {
                 this.items.unshift(this.$refs.myInput.value)
                 this.$refs.myInput.value = ''
                 console.log(event.key)
+            },
+            remove(i) {
+                this.items.splice(i, 1)
+            },
+            log(item) {
+                console.log('log item: ', item)
             }
-    },
+        },
         computed: {
             evenItems() {
                 return this.items.filter(i => i % 2 === 0)
