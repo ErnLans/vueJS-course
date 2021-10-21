c65f9f938413bbd3192d92436a9ba738513c2c6e
ErnLans <lans1965ern@gmail.com>
2021-10-21 12:45:33 +0300

    07-filter

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 Vue.createApp({
-    data: () => ({
-        myHtml: '<h1>Vue 3 App</h1>',
-        title: 'проверочка',
-        person: {
-            firstName: ' Ernest',
-            lastName: ' Lans',
-            ade: 56
-        },
-        items: [1,2,3,4,5,6]
-    })
-/*    methods: {
-        stopPropagation(event) {
-            event.stopPropagation()
+        data: () => ({
+            myHtml: '<h1>Vue 3 App</h1>',
+            title: 'проверочка',
+            person: {
+                firstName: ' Ernest',
+                lastName: ' Lans',
+                ade: 56
+            },
+            items: [1, 2, 3, 4, 5, 6]
+        }),
+        computed: {
+            evenItems() {
+                return this.items.filter(i => i % 2 === 0)
+            }
         }
-    }*/
-}).mount('#app')
+    }
+).mount('#app')
