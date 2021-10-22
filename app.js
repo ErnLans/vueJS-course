153ca642c7861303e87c3bd5272df4c72255bcc0
ErnLans <lans1965ern@gmail.com>
2021-10-22 17:27:05 +0300

    Virtual DOM and Proxy

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-Vue.createApp({
-        data: () => ({
-            myHtml: '<h1>Vue 3 App</h1>',
-            title: 'проверочка',
-            person: {
-                firstName: ' Ernest',
-                lastName: ' Lans',
-                ade: 56
-            },
-            items: [1, 2]
-        }),
-        methods: {
-            addItem(event) {
-                this.items.unshift(this.$refs.myInput.value)
-                this.$refs.myInput.value = ''
-                console.log(event.key)
-            },
-            remove(i) {
-                this.items.splice(i, 1)
-            },
-            log(item) {
-                console.log('log item: ', item)
-            }
-        },
-        computed: {
-            evenItems() {
-                return this.items.filter(i => i % 2 === 0)
-            }
+const h = Vue.h
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'Это из свойства template'
         }
+    },
+    methods: {
+        changeTitle(){
+            console.log(this.title)
+            this.title = 'Изменили!'
+        }
+    },
+    /*template:`
+    <div class="card center">
+    <h1>{{ title }}</h1>
+    <button class="btn" @click="title = 'Изменили!'">Изменить</button>
+    `*/
+    render() {
+        return h('div', {
+            class: 'card center'
+        }, [
+            h('h', {}, this.title),
+            h('button', {
+                class: 'btn',
+                onclick: this.changeTitle
+            }, 'Изменить')
+        ])
     }
-).mount('#app')
+})
+
+app.mount('#app')
